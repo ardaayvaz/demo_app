8f7654bee262c9f551fdf58543fb435ac8592947
Arda Ayvaz <ardaayvaz@gmail.com>
2013-02-19 14:23:25 -0800

    calismaya devam

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
-  belongs_to :user
+  belongs_to :user 
   validates :content, :length => {:maximum => 140}
+  #micropost tamamdir
 end
