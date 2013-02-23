29eb1d19d2e97f50323456e6de1876991ec72226
Arda Ayvaz <ardaayvaz@gmail.com>
2013-02-22 16:19:10 -0800

    durmak yok yola devam

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
-  has_many :microposts #usera micropost tanimliyoruz herhalde burda
+  has_many :microposts #usera micropost candir
 end
